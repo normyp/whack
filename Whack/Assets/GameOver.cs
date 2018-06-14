635fe482466f5ff78b889c09ad14640155fa2abb
Paul Normington <pm.normington@gmail.com>
2018-06-14 15:02:10 +0100

    Untracked files added, game over linked to lives

---
+++ Whack/Assets/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	GameObject gameMan;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject gameMan = GameObject.FindWithTag("gameMan");
+		lives player = gameMan.GetComponent<lives>();
+       	int life = player._lives;
+	}
+}
