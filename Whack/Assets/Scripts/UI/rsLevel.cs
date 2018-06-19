540541262c7b3f22b0e33f8974e6ac666be05db3
Paul Normington <pm.normington@gmail.com>
2018-06-19 12:05:05 +0100

    Game restarts after death screen shows

---
+++ Whack/Assets/Scripts/UI/rsLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rsLevel : MonoBehaviour {
+
+	public float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer >= 3.0f)
+		{
+			Application.LoadLevel("Whack!");
+		}
+		timer += Time.deltaTime;
+	}
+}
---
+++ Whack/Assets/restart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restart : MonoBehaviour {
+	
+ 	public float timer = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if(timer >= 3.0f)
+		{
+			Application.LoadLevel("Whack!");
+		}
+		timer += Time.deltaTime;
+	}
+}
