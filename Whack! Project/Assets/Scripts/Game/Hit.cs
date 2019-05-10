0e6262e759ebdaf447eccbd415185d71a904c7cb
normyp <pm.normington@gmail.com>
2019-05-10 12:43:30 +0100

    Added sprite

---
+++ Whack! Project/Assets/Scripts/Game/Hit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hit : MonoBehaviour {
+
+    public GameObject moleMan;
+    public bool whacked;
+
+    void OnMouseDown()
+    {
+        GameObject moleMan = GameObject.FindWithTag("man"); //Creates an instance of mole manager so that the component spawner can be used
+        spawner spawn = moleMan.GetComponent<spawner>(); //Creates an instance of the spawner script so that score can be accessed
+
+        //missed = false;
+
+        gameObject.SetActive(false);
+        spawn.score++; 
+        whacked = true;
+
+        //Need to override default spawn system and just make a new mole
+       
+        //Debug.Log("Hit");   
+    }
+
+    /*public bool Missed()
+    {
+    	if(missed)
+    	{
+    		check = true;
+    	}
+    	else
+    	{
+    		check = false;
+    	}
+    	return check;
+    }*/
+}
---
+++ Whack! Project/Assets/Scripts/Game/enablePoof.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enablePoof : MonoBehaviour
+{
+    public bool hit = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // gameObject.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        hit = true;
+        if(hit = true)
+        {
+            //Play animation once then setactive to false
+        }
+        //gameObject.Color.a = 0.0f;
+    }
+
+    void OnMouseUp()
+    {
+        hit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.SetActive(true);
+        /* if hit occurs
+         *setactive to true
+         * play animation
+         * then set active to false
+         */
+    }
+}
